8fb4200eb693ec41289c708272a5fc2302632482
Christopher Zauderer-Hargrove <cczauder@cougarnet.uh.edu>
2022-03-12 20:11:42 -0600

    added new component for customer detail screen

---
+++ hargrove-blades/src/components/CustomerDetail.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <!-- Website information goes here !-->
+    </div>
+</template>
+
+<script>
+export default ({
+    data() {
+        return {
+            //return data here
+        }
+    },
+
+    created() {
+        //get information from DB with axios here
+    },
+
+    methods: {
+        //js functions go here
+    }
+})
+</script>
