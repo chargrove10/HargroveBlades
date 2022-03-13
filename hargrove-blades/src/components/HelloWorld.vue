166fa5e4e61ec3087c850f14cc2e1434271214d2
Christopher Zauderer-Hargrove <cczauder@cougarnet.uh.edu>
2022-03-12 19:51:39 -0600

    comit

--- hargrove-blades/src/components/HelloWorld.vue
+++ hargrove-blades/src/components/HelloWorld.vue
@@ -1,58 +1,257 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="main-title">
+        <h1 class="title">Hargrove-Blades</h1>
+    </div>
+
+    <div class="main-content">
+
+        <div class="tab-div1">
+
+            <div class="tab">
+                
+                <button onclick ="openCustomer(event, 'Customer')">Customer</button>
+                <div class="tab-divider"></div>
+                <button onclick ="openOrder(event, 'Order')">Order</button>
+                <div class="tab-divider"></div>
+                <button onclick ="openProduct(event, 'Product')">Product</button>
+            </div>
+
+        </div>
+
+        <div class="tab-div2">
+
+            <div id="Order" class="tabcontent">
+                <div style="background-color:lightgray; text-align:center"><header>Order Information</header></div>
+                <div class="tab-divider"></div>
+                <div>
+                    <div class="cform1">
+                        <label>Order Number</label>
+                        <input type="text" id="orderNum" name="OrderNum" value="AE12321T"/><br/>
+                    </div>
+                    <div class="cform2">
+                        <label>Order Date</label>
+                        <input type="text" id="date" name="date" value="03/2022" /><br />
+                    
+                    </div>
+                    <div>
+                        <button>Search</button>
+                    </div>
+                    <div class="tab-divider"></div>
+                </div>
+                <div class="tab-divider"></div>
+                
+                <form>
+                    <table>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Order Number</th>
+                            <th>Order Date</th>
+                            <th>Order Total</th>
+                            <th>Balance</th>
+                        </tr>
+                        <tr>
+                            <td>John</td>
+                            <td>Doe</td>
+                            <td>AE1342R</td>
+                            <td>03/2022</td>
+                            <td>1</td>
+                            <td>$324</td>
+                        </tr>
+                    </table>
+                </form>
+
+                <div class="tab-divider"></div>
+                <button>Edit</button>
+
+            </div>
+
+            <div id="Customer" class="tabcontent" style="display:block">
+                <div style="background-color:lightgray; text-align:center"><header>Customer Information</header></div>
+                
+                <div class="tab-divider"></div>
+                <div>
+                    <div class="cform1">
+                        <label>Customer Name</label>
+                        <input type="text" id="fname" name="fname" value="John"/><br/>
+                    </div>
+                    <div class="cform2">
+                        <label>Phone Number</label>
+                        <input type="text" id="phone" name="phone" value="1233334565" /><br />
+                    
+                    </div>
+                    <div>
+                        <button>Search</button>
+                    </div>
+                    <div class="tab-divider"></div>
+                </div>
+                <div class="tab-divider"></div>
+           
+                <div>
+                    <form>
+                        <table>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Phone Number</th>
+                                <th>City</th>
+                                <th>State</th>
+                                
+                                
+                            </tr>
+                           
+                            <tr>
+                                <td>John</td>
+                                <td>Doe</td>
+                                <td>1234454321</td>
+                                <td>Sugar Grove</td>
+                                <td>Texas</td>
+                                
+                                
+                            </tr>
+                            
+                        </table>
+                    </form>
+                </div>
+                <div class="tab-divider"></div>
+
+                <div>
+                    <button >Edit</button>
+                    <div class="divider "></div>
+                    <button >Add</button>
+                    <div class="divider"></div>
+                    <button >Create Order</button>
+                </div>
+
+            </div>
+
+            <div id="Product" class="tabcontent">
+                <div style="background-color:lightgray; text-align:center"><header>Product Information</header></div>
+                <div class="tab-divider"></div>
+                <div>
+                    <div class="pform1">
+                        <label>Serial Number</label>
+                        <input type="text" id="serial" name="serial" value="1234era54t"/><br/>
+                    </div>
+                    <div class="pform2">
+                        <label>Knife Style</label>
+                        <input type="text" id="kstyle" name="kstyle" value="Curved" /><br/>               
+                    </div>
+                    <div class="pform3">
+                        <label>Steel Type</label>
+                        <input type="text" id="stype" name="stype" value="Damascus" /><br/>
+                    </div>
+                    <div>
+                        <button>Search</button>
+                    </div>
+                    <div class="tab-divider"></div>
+                </div>
+                <div class="tab-divider"></div>
+           
+                <div>
+                    <form>
+                        <table>
+                            <tr>
+                                <th>Serial Number</th>
+                                <th>Product Status</th>
+                                <th>Knife Style</th>
+                                <th>Steel Type</th>
+                                <th>Handle Material</th>
+                                <th>Blade Length</th>
+                                <th>Overall Length</th>
+                                <th>Embellishments</th>
+                            </tr>
+                            <tr>
+                                <td>AE12344e</td>
+                                <td>Pending</td>
+                                <td>Test</td>
+                                <td>Test</td>
+                                <td>Test</td>
+                                <td>Test</td>
+                                <td>Test</td>
+                                <td>Test</td>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
+                <div class="tab-divider"></div>
+
+                <div>
+                    <button >Edit</button>
+                    <div class="divider "></div>
+                    <button >Add</button>
+                </div>
+            </div>
+        
+        </div>
+        
+    </div>
 </template>
 
 <script>
 export default {
+
+        methods: {
+
+            openOrder(event, Order) {
+        var i, tabcontent, tablink;
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablink = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablink.length; i++) {
+            tablink[i].className = tablink[i].className.replace("active", "");
+        }
+
+        document.getElementById(Order).style.display = "block";
+        event.currentTarget.className += "active";
+            },
+
+            openCustomer(event, Customer) {
+        var i, tabcontent, tablink;
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablink = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablink.length; i++) {
+            tablink[i].className = tablink[i].className.replace("active", "");
+        }
+
+        document.getElementById(Customer).style.display = "block";
+        event.currentTarget.className += "active";
+            },
+
+        openProduct(event, Product) {
+        var i, tabcontent, tablink;
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablink = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablink.length; i++) {
+            tablink[i].className = tablink[i].className.replace("active", "");
+        }
+
+        document.getElementById(Product).style.display = "block";
+        event.currentTarget.className += "active";
+            }
+
+        },
+
   name: 'HelloWorld',
   props: {
     msg: String
   }
-}
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped src="../assets/style.css"></style>
