b21d3280eed9a4a792f4373c5b55d776dca7f64f
Christopher Zauderer-Hargrove <cczauder@cougarnet.uh.edu>
2022-03-16 17:21:41 -0500

    Added reports and more functonality

---
+++ hargrove-blades/src/components/Reports.vue
@@ -0,0 +1,315 @@
+<template>
+<div>
+    <div class="main-title">
+        <h1 class="title">Hargrove-Blades Reports</h1>
+    </div>
+
+    <div class="main-content">
+
+        <div class="tab-div1">
+
+            <div class="tab" style="transform: translate(20%, 10%)">
+                
+                <button v-on:click = home()>Home</button>
+                <div class="tab-divider" />
+                <button v-on:click ="openKnifeAvail(event, 'knifeAvail')">Availability</button>
+                <div class="tab-divider" />
+                <button v-on:click ="openOrderInfo(event, 'OrderInfo')">Order Info</button>
+                <div class="tab-divider" />
+                <button v-on:click ="openNewsletter(event, 'Newsletter')">Newsletter</button>
+                <div class="tab-divider" />
+                <button v-on:click ="openPriceEstimate(event, 'priceEstimate')">Price Estimate</button>
+                
+            </div>
+
+        </div>
+
+        <div class="tab-div2">
+
+            <div id="knifeAvail" class="tabcontent">
+                
+                <div style="background-color:lightgray; text-align:center"><header>Available Knives</header></div>
+                
+                <form>
+                    <table>
+                        <tr>
+                            <th>Order Number</th>
+                            <th>Order Status</th>
+                            <th>Product Status</th>
+                            <th>Knife Style</th>
+                            <th>Steel Used</th>
+                            <th>Blade Length</th>
+                            <th>Overall Length</th>
+                            <th>Blade Finish</th>
+                            <th>Handle Material</th>
+                            <th>Price</th>
+                        </tr>
+                        <tr>
+                            <td>166ae4tr</td>
+                            <td>Cancelled</td>
+                            <td>Available</td>
+                            <td>Kitchen</td>
+                            <td>Steel</td>
+                            <td>9</td>
+                            <td>14</td>
+                            <td>Polished</td>
+                            <td>Redwood</td>
+                            <td>$308</td>
+                        </tr>
+                    </table>
+                </form>
+
+
+            </div>
+
+            <div id="OrderInfo" class="tabcontent">
+                
+                <div style="background-color:lightgray; text-align:center"><header>Order Information</header></div>
+                
+                <form>
+                    <table>
+                        <tr>
+                            <th>Order Number</th>
+                            <th>Order Date</th>
+                            <th>Order Status</th>
+                            <th>Product Status</th>
+                            <th>Complete Date</th>
+                            
+                        </tr>
+                        <tr>
+                            <td>166ae4tr</td>
+                            <td>03/2022</td>
+                            <td>In Progress</td>
+                            <td>Pending</td>
+                            <td>null</td>
+                            
+                        </tr>
+                    </table>
+                </form>
+
+            </div>
+
+            <div id="Newsletter" class="tabcontent">
+                
+                <div style="background-color:lightgray; text-align:center"><header>Newsletter</header></div>
+                
+                <div class="tab-divider"></div>
+                <div>
+                    <div class="cform1">
+                        <label>Knife Style</label>
+                        <input type="text" id="styleName" name="styleName" value="Test"/><br/>
+                    </div>
+                    <div class="cform2">
+                        <label>Steel Description</label>
+                        <input type="text" id="steelDesc" name="steelDesc" value="Test" /><br />
+                    
+                    </div>
+
+                    <div class="tab-divider"></div>
+                </div>
+
+                <div class="tab-divider"></div>
+
+                <div class="search-button">
+                        <button>Search</button>
+                </div>
+                
+                <div class="tab-divider"></div>
+                
+                <form>
+                    <table>
+                        <tr>
+                            <th>Customer First Name</th>
+                            <th>Customer Last Name</th>
+                            <th>Customer Email</th>
+                            <th>Knife Style</th>
+                            <th>Steel Type</th>
+
+                        </tr>
+                        <tr>
+                            <td>John</td>
+                            <td>Doe</td>
+                            <td>something@gmail.com</td>
+                            <td>Butcher's Knife</td>
+                            <td>Random Type</td>
+                        </tr>
+                    </table>
+                </form>
+
+            </div>
+
+            <div id="priceEstimate" class="tabcontent">
+                
+                <div style="background-color:lightgray; text-align:center"><header>Price Estimate</header></div>
+                
+                <div class="tab-divider"></div>
+                <div>
+                    <div class="pform1">
+                        <label>Knife Style</label>
+                        <input type="text" id="styleName" name="styleName" value="Test"/><br/>
+                    </div>
+                    <div class="pform2">
+                        <label>Steel Type</label>
+                        <input type="text" id="steelType" name="steelType" value="Test" /><br />
+                    
+                    </div>
+                    <div class="pform3">
+                        <label>Handle Material</label>
+                        <input type="text" id="handleMat" name="handleMat" value="Test" /><br />
+                    
+                    </div>
+
+                    <div class="tab-divider"></div>
+                </div>
+
+                <div class="tab-divider"></div>
+
+                <div class="search-button">
+                        <button>Search</button>
+                </div>
+                
+                <div class="tab-divider"></div>
+
+                <form>
+                    <table>
+                        <tr>
+                            <th>Knife Style</th>
+                            <th>Steel Type</th>
+                            <th>Handle Material</th>
+                            <th>Embellishment</th>
+                            <th>Blade Length</th>
+                            <th>Blade Finish</th>
+                            <th>Overall Length</th>
+                            <th>Price</th>
+                        </tr>
+                        
+                        <tr>
+                            <td>Kitchen</td>
+                            <td>Damascus</td>
+                            <td>Redwood</td>
+                            <td>Branding</td>
+                            <td>8</td>
+                            <td>Polished</td>
+                            <td>13</td>
+                            <td>350</td>
+                        </tr>
+                    </table>
+                </form>
+
+            </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+
+</template>
+
+<script>
+
+export default {
+
+
+        methods: {
+
+        home() {
+            this.$router.push('/')
+        },
+
+        openKnifeAvail(event, KnifeAvail) {
+        var i, tabcontent, tablink;
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablink = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablink.length; i++) {
+            tablink[i].className = tablink[i].className.replace("active", "");
+        }
+
+        document.getElementById(KnifeAvail).style.display = "block";
+        event.currentTarget.className += "active";
+        
+        },
+
+        openOrderInfo(event, OrderInfo) {
+        var i, tabcontent, tablink;
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablink = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablink.length; i++) {
+            tablink[i].className = tablink[i].className.replace("active", "");
+        }
+
+        document.getElementById(OrderInfo).style.display = "block";
+        event.currentTarget.className += "active";
+        },
+
+        openNewsletter(event, Newsletter) {
+        var i, tabcontent, tablink;
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablink = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablink.length; i++) {
+            tablink[i].className = tablink[i].className.replace("active", "");
+        }
+
+        document.getElementById(Newsletter).style.display = "block";
+        event.currentTarget.className += "active";
+        },
+
+        openPriceEstimate(event, priceEstimate) {
+        var i, tabcontent, tablink;
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablink = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablink.length; i++) {
+            tablink[i].className = tablink[i].className.replace("active", "");
+        }
+
+        document.getElementById(priceEstimate).style.display = "block";
+        event.currentTarget.className += "active";
+        },
+
+        openReport5(event, Report5) {
+        var i, tabcontent, tablink;
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablink = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablink.length; i++) {
+            tablink[i].className = tablink[i].className.replace("active", "");
+        }
+
+        document.getElementById(Report5).style.display = "block";
+        event.currentTarget.className += "active";
+        }
+
+        
+
+        },
+
+    }
+</script>
+
+
