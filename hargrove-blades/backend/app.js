c1ded10fb1d39f85a484edab004f51b165379997
Christopher Zauderer-Hargrove <cczauder@cougarnet.uh.edu>
2022-03-20 21:47:58 -0500

    added cors and query

--- hargrove-blades/backend/app.js
+++ hargrove-blades/backend/app.js
@@ -2,37 +2,49 @@ const express = require('express');
 const sql = require('mssql');
 const bodyParser = require('body-parser');
 var router = express.Router();
+const cors = require('cors')
 
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser)
+app.use(cors())
 
 app.listen(PORT, () => {
     console.log("Connection achieved on : ", PORT)
 })
 
-const database = {
-    username: '',
-    password: '',
-    server: 'server',
-    database: 'databse',
+var config = {
+    server: 'COT-CIS4375-01',
+    database: 'HargroveBlades',
+    user: 'ISSD',
+    password: 'HargroveBlades@!',
+    port: 1433,
     options: {
-        instancename: 'SQLEXPRESS' //This tells its a SQL Server instance
-    },
-    port: 1433
+        trustServerCertificate: true // change to true for local dev / self-signed certs
+      }
+};
+
+function listProducts() {
+    var conn = new sql.ConnectionPool(config);
+    conn.connect().then(function () {
+        var request = new sql.Request(conn);
+        request.query("SELECT CUSTOMER.CustomerFirstName, CUSTOMER.CustomerLastName, CUSTOMER.CustomerPhone " +
+        "FROM CUSTOMER "+ 
+        "JOIN CUSTOMERSTATUS ON CUSTOMER.CUSTOMERSTATUSID = CUSTOMERSTATUS.CUSTOMERSTATUSID").then(function (recordSet) {
+            console.log(recordSet);
+            conn.close();
+        }).catch(function (err) {
+            console.log(err);
+            conn.close();
+        });
+    }).catch(function (err) {
+        console.log(err);
+    });
 }
 
-sql.connect(database, err => {
-
-
-    if (err) {
-        console.log('Not Connected')
-    } else {
-        console.log('Connected')
-    }
-})
+listProducts();
 
 app.get('/', (req,res) => {
     console.log(res)
@@ -41,11 +53,11 @@ app.get('/', (req,res) => {
 })
 
 router.get('/customerList', function (req, res) {
-    sql.connect(database).then(() => {
+    sql.connect(config).then(() => {
         //Query selects everything but we selecting specifics for tables
         return sql.query("SELECT CUSTOMER.CustomerFirstName, CUSTOMER.CustomerLastName, CUSTOMER.CustomerPhone, ADDRESS.City, STATE.StateName "+ 
-        "FROM CUSTOMER JOIN ADDRESS ON CUSTOMER.CUSTOMERID=ADDRESS.CUSTOMERID" +  
-        "JOIN CUSTOMERSTATUS ON CUSTOMER.CUSTOMERSTATUSID = CUSTOMERSTATUS.CUSTOMERSTATUSID" + 
+        "FROM CUSTOMER JOIN ADDRESS ON CUSTOMER.CUSTOMERID=ADDRESS.CUSTOMERID " +  
+        "JOIN CUSTOMERSTATUS ON CUSTOMER.CUSTOMERSTATUSID = CUSTOMERSTATUS.CUSTOMERSTATUSID " + 
         "JOIN STATE ON ADDRESS.STATEID=STATE.STATEID");
     }).then (data => {
         res.send(data.recordset);
@@ -59,11 +71,11 @@ router.get('/customerList/:customerName/:customerPhone', function (req, res) {
     const name = req.params.customerName
     const phone = req.params.customerPhone
 
-    sql.connect(database).then(() => {
+    sql.connect(config).then(() => {
         //Query selects everything but we selecting specifics for tables
         return sql.query("SELECT CUSTOMER.CustomerFirstName, CUSTOMER.CustomerLastName, CUSTOMER.CustomerPhone, ADDRESS.City, STATE.StateName "+ 
-        "FROM CUSTOMER JOIN ADDRESS ON CUSTOMER.CUSTOMERID=ADDRESS.CUSTOMERID" +  
-        "JOIN CUSTOMERSTATUS ON CUSTOMER.CUSTOMERSTATUSID = CUSTOMERSTATUS.CUSTOMERSTATUSID" + 
+        "FROM CUSTOMER JOIN ADDRESS ON CUSTOMER.CUSTOMERID=ADDRESS.CUSTOMERID " +  
+        "JOIN CUSTOMERSTATUS ON CUSTOMER.CUSTOMERSTATUSID = CUSTOMERSTATUS.CUSTOMERSTATUSID " + 
         "JOIN STATE ON ADDRESS.STATEID=STATE.STATEID WHERE CUSTOMER.CustomerLastName = ? AND CUSTOMER.CustomerPhone = ?", name, phone);
     }).then (data => {
         res.send(data.recordset);
@@ -74,12 +86,12 @@ router.get('/customerList/:customerName/:customerPhone', function (req, res) {
 
 
 router.get('/productList', function (req, res) {
-    sql.connect(database).then(() => {
+    sql.connect(config).then(() => {
         //Query selects everything but we selecting specifics for tables
-        return sql.query("SELECT P.SerialNo, PS.ProductStatusName, style.StyleName, steel.SteelName" + 
-        "P.HandleMaterial, P.BladeLength, P.OverallLength, P.Embellishments" +
-        "FROM Product P JOIN KnifeStyle style ON P.StyleID = style.StyleId" + 
-        "JOIN KnifeSteel steel ON P.SteelID = steel.SteelID" + 
+        return sql.query("SELECT P.SerialNo, PS.ProductStatusName, style.StyleName, steel.SteelName " + 
+        "P.HandleMaterial, P.BladeLength, P.OverallLength, P.Embellishments " +
+        "FROM Product P JOIN KnifeStyle style ON P.StyleID = style.StyleId " + 
+        "JOIN KnifeSteel steel ON P.SteelID = steel.SteelID " + 
         "JOIN ProductStatus PS ON P.ProductStatusId = PS.ProductStatusID");
     }).then (data => {
         res.send(data.recordset);
@@ -89,9 +101,9 @@ router.get('/productList', function (req, res) {
 });
 
 router.get('/orderList', function (req, res) {
-    sql.connect(database).then(() => {
+    sql.connect(config).then(() => {
         //Query selects everything but we selecting specifics for tables
-        return sql.query("SELECT C.CustomerFirstName, C.CustomerLastName, PO.OrderNumber, PO.OrderDate, PO.OrderTotal, PO.Balance"+
+        return sql.query("SELECT C.CustomerFirstName, C.CustomerLastName, PO.OrderNumber, PO.OrderDate, PO.OrderTotal, PO.Balance "+
         "FROM PRODUCTORDER PO JOIN CUSTOMER C ON C.CustomerID = PO.CustomerID");
     }).then (data => {
         res.send(data.recordset);
