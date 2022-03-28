0e70862434c6073a8bfb7f2498d814e8367ada00
Christopher Zauderer-Hargrove <cczauder@cougarnet.uh.edu>
2022-03-27 20:20:27 -0500

    test

--- hargrove-blades/src/components/ProductDetail.vue
+++ hargrove-blades/src/components/ProductDetail.vue
@@ -25,15 +25,21 @@
                         <label for="serialNo">Serial Number </label><br/>
                         <input type="text" id="serialNo" name="serialNo" value="1234era54t"/><br/>
                         <label for="prodStatus">Product Status </label><br/>
-                        <input type="text" id="prodStatus" name="prodStatus" value="Pending"/><br/>
+                        <select name="ps" id ="ps" @change="prodStatus($event)" v-model="product.ProductStatusName">
+                            <option v-for="productDropDown in ProductDropDown" v-bind:value="productDropDown.ProductStatusID" :key="productDropDown.ProductStatusID"> {{productDropDown.ProductStatusName}} </option>
+                        </select><br/>
                         <label for="knifeStyle">Knife Style</label><br/>
-                        <input type="text" id="knifeStyle" name="knifeStyle" value="Test"/><br/>
+                        <select name="ks" id ="ks" @change="knifeStatus($event)" v-model="product.StyleName">
+                            <option v-for="knifeDropDown in KnifeDropDown" v-bind:value="knifeDropDown.StyleID" :key="knifeDropDown.StyleID"> {{knifeDropDown.StyleName}} </option>
+                        </select><br/>
                         <label for="steelType">Steel Type</label><br/>
-                        <input type="text" id="steelType" name="steelType" value="Test"/><br/> 
+                        <select name="st" id ="st" @change="steelStatus($event)" v-model="product.SteelName">
+                            <option v-for="steelDropDown in SteelDropDown" v-bind:value="steelDropDown.SteelID" :key="steelDropDown.SteelID"> {{steelDropDown.SteelName}} </option>
+                        </select><br/> 
                         <label for="price">Price</label><br/>
                         <input type="text" id="price" name="price" value="Test"/><br/> 
                         <label for="completeDate">Complete Date</label><br/>
-                        <input type="date" id="completeDate" name="completeDate"/><br/>    
+                        <input type="date" id="completeDate" name="completeDate" /><br/>    
                     </form>
                 </div>
 
@@ -62,13 +68,99 @@
 </template>
 
 <script>
-
+import axios from 'axios'
     export default {
+        
+        data() {
+            return {
+                Products: [],
+                isShow: false,
+                product: {
+                    ProductID: '',
+                    StyleID: '',
+                    SteelID: '',
+                    ProductStatusID: '',
+                    CompleteDate: '',
+                    Price: '',
+                    SerialNo: '',
+                    OverallLength: '',
+                    BladeFinish: '',
+                    BladeLength: '',
+                    Embellishments: '',
+                    HandleMaterial: '',
+                    ProductNotes: '',
+                    ProductStatusName: '',
+                    StyleName: '',
+                    SteelName: ''                   
+                },
+                ProductDropDown: [],
+                productDropDown: {
+                    ProductStatusID: '',
+                    ProductStatusName: ''
+                },
+                KnifeDropDown: [],
+                knifeDropDown: {
+                    StyleID: '',
+                    StyleName: ''
+                },
+                SteelDropDown: [],
+                steelDropDown: {
+                    SteelID: '',
+                    SteelName: ''
+                }
+            }
+        },
+
+        created() {
+            let url = 'http://localhost:3000/product33';
+
+            axios.get(url).then((response) => {
+                this.Products = response.data
+                console.log(this.Products)
+            }).catch(err => {
+                console.log(err)
+            });
+
+            axios.get('http://localhost:3000/productDropDown').then((statusDropDown) => {
+                this.ProductDropDown = statusDropDown.data
+            }).catch(err => {
+                console.log(err)
+            });
+
+            axios.get('http://localhost:3000/knifeDropDown').then((knifeDropDown) => {
+                this.KnifeDropDown = knifeDropDown.data
+            }).catch(err => {
+                console.log(err)
+            });
+
+            axios.get('http://localhost:3000/steelDropDown').then((steelDropDown) => {
+                this.SteelDropDown = steelDropDown.data
+            }).catch(err => {
+                console.log(err)
+            });
+        },
+
         methods: {
             home() {
                 this.$router.push('/customerList')
+            },
+
+            prodStatus(e) {
+                this.product.ProductStatusID = e.target.value
+                console.log(this.product.ProductStatusID)
+            },
+
+            knifeStatus(e) {
+                this.product.StyleID = e.target.value
+                console.log(this.product.StyleID)
+            },
+
+            steelStatus(e) {
+                this.product.SteelID = e.target.value
+                console.log(this.product.SteelID)
             }
-        }
+        }       
+        
     }
 
 </script>
